56132e2144038dfece520b1478b5ba20128b14bc
unknown <theophiluseniayewu@gmail.com>
2024-07-31 01:06:26 +0100

    added feature to add hotel, and e2e test for feature

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -33,7 +33,7 @@ app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/my-hotels", myHotelRoutes);
 
-api.get("*", (req: Request, res: Response) => {
+app.get("*", (req: Request, res: Response) => {
     res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"))
 } )
 
