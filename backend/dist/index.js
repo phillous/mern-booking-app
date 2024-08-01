6bc484718dabca4605ee370a15bb9a2712aeadd4
unknown <theophiluseniayewu@gmail.com>
2024-08-01 13:04:29 +0100

    added my hotel view and e2e tests

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -11,6 +11,13 @@ const users_1 = __importDefault(require("./routes/users"));
 const auth_1 = __importDefault(require("./routes/auth"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const path_1 = __importDefault(require("path"));
+const cloudinary_1 = require("cloudinary");
+const my_hotels_1 = __importDefault(require("./routes/my-hotels"));
+cloudinary_1.v2.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 mongoose_1.default.connect(process.env.MONGODB_CONNECTION_STRING);
 const app = (0, express_1.default)();
 app.use((0, cookie_parser_1.default)());
@@ -23,6 +30,10 @@ app.use((0, cors_1.default)({
 app.use(express_1.default.static(path_1.default.join(__dirname, "../../frontend/dist")));
 app.use("/api/auth", auth_1.default);
 app.use("/api/users", users_1.default);
+app.use("/api/my-hotels", my_hotels_1.default);
+app.get("*", (req, res) => {
+    res.sendFile(path_1.default.join(__dirname, "../../frontend/dist/index.html"));
+});
 app.listen(7000, () => {
     console.log("server running on localhost:7000");
 });
